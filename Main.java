63c3601c85d7a753cd64a77315c7737b72599b84
katarina-cohen <98794760+katarina-cohen@users.noreply.github.com>
2022-03-08 11:53:19 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+*Main.java
+*
+*Runs PolynomialDriver.java
+*
+*@author Katarina Cohen
+*@version 1.0
+*
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    PolynomialDriver.main(null);
+  }
+}
+
